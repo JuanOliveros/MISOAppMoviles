b04494b6bb97f1d987f2a0a3c5fc9bca4e4ae3b6
Carlos Garcia <ci.garcia81@uniandes.edu.co>
2021-11-11 22:05:42 -0500

    Creación fragmento, item artista, repositorio, adaptador artista y ajuste al network adapter y a los modelos

--- app/src/main/java/com/example/vinilos/ui/fragments/ArtistsFragment.kt
+++ app/src/main/java/com/example/vinilos/ui/fragments/ArtistsFragment.kt
@@ -4,42 +4,67 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.vinilos.databinding.FragmentArtistsBinding
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vinilos.models.Perfomer
+import com.example.vinilos.ui.adapters.ArtistAdapter
 import com.example.vinilos.viewmodels.ArtistsViewModel
 
 class ArtistsFragment : Fragment() {
 
-    private lateinit var artistsViewModel: ArtistsViewModel
     private var _binding: FragmentArtistsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewModel: ArtistsViewModel
+    private var viewModelAdapter: ArtistAdapter? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        artistsViewModel =
-            ViewModelProvider(this).get(ArtistsViewModel::class.java)
-
         _binding = FragmentArtistsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val view = binding.root
+        viewModelAdapter = ArtistAdapter()
+        return view
+    }
 
-        val textView: TextView = binding.textArtists
-        artistsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.fragmentArtist
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = viewModelAdapter
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val activity = requireNotNull(this.activity) {
+            "You can only access the viewModel after onActivityCreated()"
+        }
+        viewModel = ViewModelProvider(this, ArtistsViewModel.Factory(activity.application)).get(
+            ArtistsViewModel::class.java)
+        viewModel.artists.observe(viewLifecycleOwner, Observer<List<Perfomer>> {
+            it.apply {
+                viewModelAdapter!!.artists = this
+            }
         })
-        return root
+        viewModel.eventNetworkError.observe(viewLifecycleOwner, Observer<Boolean> { isNetworkError ->
+            if (isNetworkError) onNetworkError()
+        })
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun onNetworkError() {
+        if(!viewModel.isNetworkErrorShown.value!!) {
+            Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+            viewModel.onNetworkErrorShown()
+        }
     }
 }
