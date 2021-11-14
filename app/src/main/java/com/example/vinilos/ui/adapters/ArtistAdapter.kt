3f0a5e7416d83756f0a315f7b1b0f86a80d25455
CiGarCiGa <ci.garcia81@uniandes.edu.co>
2021-11-13 21:02:55 -0500

    Feature/hu03 listado artistas (#50)
    * Creación fragmento, item artista, repositorio, adaptador artista y ajuste al network adapter y a los modelos
    
    * Artist detail.

---
+++ app/src/main/java/com/example/vinilos/ui/adapters/ArtistAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.vinilos.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vinilos.R
+import com.example.vinilos.databinding.ArtistItemBinding
+import com.example.vinilos.models.Perfomer
+
+class ArtistAdapter(private val onItemClicked: (position: Int) -> Unit) : RecyclerView.Adapter<ArtistAdapter.ArtistViewHolder>(){
+
+    var artists :List<Perfomer> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtistViewHolder {
+        val withDataBinding: ArtistItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            ArtistViewHolder.LAYOUT,
+            parent,
+            false)
+
+        return ArtistViewHolder(withDataBinding,onItemClicked)
+    }
+
+    override fun onBindViewHolder(holder: ArtistViewHolder, position: Int) {
+        holder.viewDataBinding.also {
+            it.artist = artists[position]
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return artists.size
+    }
+
+    class ArtistViewHolder(val viewDataBinding: ArtistItemBinding, private val onItemClicked: (position: Int) -> Unit) :
+        RecyclerView.ViewHolder(viewDataBinding.root),View.OnClickListener {
+        companion object {
+            @LayoutRes
+            val LAYOUT = R.layout.artist_item
+        }
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View) {
+            val position = adapterPosition
+            onItemClicked(position)
+        }
+
+    }
+
+}
