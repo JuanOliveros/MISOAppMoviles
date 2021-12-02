107cb1e1726e57219b5162b26a77cfd9c2db8a93
Oscar Ortiz <oscarm.ortizf@gmail.com>
2021-12-02 09:00:45 -0500

    Fixed fragment navigation to reload list

--- app/src/main/java/com/example/vinilos/ui/fragments/AlbumCreateFragment.kt
+++ app/src/main/java/com/example/vinilos/ui/fragments/AlbumCreateFragment.kt
@@ -11,7 +11,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.findNavController
 import com.example.vinilos.R
 import com.example.vinilos.databinding.FragmentAlbumCreateBinding
 import com.example.vinilos.viewmodels.AlbumCreateViewModel
@@ -28,8 +28,8 @@ class AlbumCreateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         albumCreateViewModel = ViewModelProvider(this).get(AlbumCreateViewModel::class.java)
-
         _binding = FragmentAlbumCreateBinding.inflate(inflater, container, false)
+        val view = binding.root
 
         val genreSpinner: Spinner = binding.albumGenreField
         ArrayAdapter.createFromResource(
@@ -66,7 +66,8 @@ class AlbumCreateFragment : Fragment() {
                 it.apply {
                     if (this == 200) {
                         Toast.makeText(activity, "El álbum fue creado", Toast.LENGTH_SHORT).show()
-                        findNavController().popBackStack()
+                        val action = AlbumCreateFragmentDirections.actionAlbumCreateToNavAlbums()
+                        view.findNavController().navigate(action)
                     } else {
                         Toast.makeText(activity, "¡Hubo un error!", Toast.LENGTH_SHORT).show()
                     }
@@ -74,7 +75,7 @@ class AlbumCreateFragment : Fragment() {
             })
         })
 
-        return binding.root
+        return view
     }
 
     override fun onDestroyView() {
