202310993d72a2eb2097c30fc4ca3560cd1c32ca
Marisela Del Valle <m.delvallem@uniandes.edu.co>
2021-12-04 14:01:45 -0500

    HU07 test: collectorFillsFormToCreateNewAlbumTest

--- app/src/androidTest/java/com/example/vinilos/ui/activities/TestHU07.java
+++ app/src/androidTest/java/com/example/vinilos/ui/activities/TestHU07.java
@@ -1,26 +1,17 @@
 package com.example.vinilos.ui.activities;
 
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static org.hamcrest.Matchers.anyOf;
 
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.DrawerActions;
-import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -28,11 +19,9 @@ import androidx.test.runner.AndroidJUnit4;
 import com.example.vinilos.EntranceActivity;
 import com.example.vinilos.R;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 
 @LargeTest
@@ -42,7 +31,6 @@ public class TestHU07 {
     @Rule
     public ActivityTestRule<EntranceActivity> mActivityTestRule = new ActivityTestRule<>(EntranceActivity.class);
 
-    /*
     @Test
     public void accessAsCollectorTest() throws InterruptedException {
         ViewInteraction guestBtn = onView(withId(R.id.collector_button));
@@ -123,9 +111,9 @@ public class TestHU07 {
                 .check(matches(withText("Crear álbum")));
 
     }
-     */
+
     @Test
-    public void collectorCanCreateNewAlbumTest() throws InterruptedException {
+    public void collectorFillsFormToCreateNewAlbumTest() throws InterruptedException {
         ViewInteraction guestBtn = onView(withId(R.id.collector_button));
         guestBtn.check(matches(withText("Coleccionista")));
         guestBtn.perform(click());
@@ -135,8 +123,8 @@ public class TestHU07 {
 
         onView(withText("Álbumes")).perform(click());
 
-        Thread.sleep(1000);
-/*
+        Thread.sleep(3000);
+
         onView(anyOf(withId(R.id.createAlbumButton)))
                 .perform(click());
 
@@ -146,33 +134,25 @@ public class TestHU07 {
                 .check(matches(withText("Crear álbum")));
 
         ViewInteraction nameField = onView(withId(R.id.albumNameField));
-        nameField.perform(scrollTo(), replaceText("Album pruebas Equipo 12"), closeSoftKeyboard());
+        nameField.perform(scrollTo(), replaceText("Album de prueba Equipo 12"), closeSoftKeyboard());
 
         ViewInteraction coverField = onView(withId(R.id.albumCoverField));
-        coverField.perform(scrollTo(), replaceText("Url for cover"), closeSoftKeyboard());
+        coverField.perform(scrollTo(), replaceText("Url for new cover"), closeSoftKeyboard());
 
         ViewInteraction dateField = onView(withId(R.id.albumReleaseDateField));
-        dateField.perform(scrollTo(), replaceText("12-20-1990"), closeSoftKeyboard());
+        dateField.perform(scrollTo(), replaceText("12-20-1993"), closeSoftKeyboard());
 
         ViewInteraction descriptionField = onView(withId(R.id.albumDescriptionField));
-        descriptionField.perform(scrollTo(), replaceText("Descripcion de pruebas"), closeSoftKeyboard());
+        descriptionField.perform(scrollTo(), replaceText("Descripcion del album de prueba"), closeSoftKeyboard());
 
         onView(anyOf(withId(R.id.submitAlbumCreateButton)))
                 .perform(click());
 
-        Thread.sleep(2000);
+        Thread.sleep(1500);
 
-        // hasta aqui todo bien
+        onView(withId(R.id.createAlbumButton))
+                .check(matches(withText("Crear álbum")));
 
-        onView(withId(R.id.fragment_album)).perform(RecyclerViewActions.scrollToPosition(-1));
-        Thread.sleep(20000);
-        onView(withId(R.id.fragment_album)).perform(RecyclerViewActions.actionOnItemAtPosition(-1, click()));
-*/
-        Thread.sleep(4000);
-
-        onView(withId(R.id.fragment_album))
-                .check(matches(hasDescendant(withText("Album pruebas Equipo 12"))));
-
+        // se valida que el formulario se llena exitosamente y vuelve al listado de albumes
     }
-
 }
